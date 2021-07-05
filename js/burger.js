cb5cd24c9c8281a6af81f2db6542b8364d13ad5c
Alex McKendrick <48360400+Hexagonsun70@users.noreply.github.com>
2021-07-05 11:43:41 +0100

    getting burger menu working properly, about to change many classes

--- js/burger.js
+++ js/burger.js
@@ -3,28 +3,29 @@ $(".hamburger").click(function () {
   if ($(".hamburger").hasClass("is-active")) {
     $(".hamburger").removeClass("is-active");
     console.log("Class should be removed")
-    $("body").removeClass("burger-shift-left");
-    $("body").addClass("burger-shift-right");
+    $("header, main, footer").removeClass("burger-shift-left");
+    $("header, main, footer").addClass("burger-shift-right");
   } else {
     $(".hamburger").addClass("is-active");
     console.log("Class should be added")
-    $("body").removeClass("burger-shift-right");
-    $("body").addClass("burger-shift-left");
+    $("header, main, footer").removeClass("burger-shift-right");
+    $("header, main, footer").addClass("burger-shift-left");
     $(".mask-click").addClass("hamburger-mask");
   };
 });
 
-$(".hamburger-mask").click(function () {
+$(".mask-click").click(function () {
   if ($(".hamburger").hasClass("is-active")) {
     $(".hamburger").removeClass("is-active");
     console.log("Class should be removed")
-    $("body").removeClass("burger-shift-left");
-    $("body").addClass("burger-shift-right");
+    $("header, main, footer").removeClass("burger-shift-left");
+    $("header, main, footer").addClass("burger-shift-right");
+    $(".mask-click").removeClass("hamburger-mask");
   } else {
     $(".hamburger").addClass("is-active");
     console.log("Class should be added")
-    $("body").removeClass("burger-shift-right");
-    $("body").addClass("burger-shift-left");
+    $("header, main, footer").removeClass("burger-shift-right");
+    $("header, main, footer").addClass("burger-shift-left");
     $(".mask-click").addClass("hamburger-mask");
   };
 });
