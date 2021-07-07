c26fe5a25d73d31f47fe569a9f28afffdb513d5a
Alex McKendrick <48360400+Hexagonsun70@users.noreply.github.com>
2021-07-07 20:22:16 +0100

    burger menu bugs fixed, styling sidebar. need to re-calculate JS values

--- js/burger.js
+++ js/burger.js
@@ -1,31 +1,67 @@
+class TempScrollBox {
+  constructor() {
+    this.scrollBarWidth = 0;
+    this.measureScrollbarWidth();
+  }
+  measureScrollbarWidth() {
+    // Add temporary box to wrapper
+    let scrollbox = document.createElement('div');
+    // Make box scrollable
+    scrollbox.style.overflow = 'scroll';
+    // Append box to document
+    document.body.appendChild(scrollbox);
+    // Measure inner width of box
+    this.scrollBarWidth = scrollbox.offsetWidth - scrollbox.clientWidth;
+    // Remove box
+    document.body.removeChild(scrollbox);
+  }
+  get width() {
+    return this.scrollBarWidth;
+  }
+}
+
+// class MeasureSidebar {
+//   constructor() {
+//     this.sidebarWidth = 0;
+//     this.measureSidebarWidth();
+//   }
+//   measureSidebarWidth(){
+//     let sidebar = document.getElementById("sb-width");
+//     this.sidebarWidth = sidebar.offsetWidth - sidebar.clientWidth;
+//   }
+//   get widthTwo() {
+//     return this.sidebarWidth;
+//   }
+// }
+
+let scrollbox = new TempScrollBox();
+let transformStart  = `translateX(${scrollbox.width}px + 257px)`;
+let transformLeft = `translateX(0)`;
+
+
+
+console.log(scrollbox.width); //-> 17 (Windows, Chrome)
+console.log(transformStart); //-> translateX(17px + 257px) (Windows, Chrome)
+
+const shiftContent = $("header, main, footer");
+
 $(".hamburger").click(function () {
-  console.log("Hamburger Clicked");
-  if ($(".hamburger").hasClass("is-active")) {
-    $(".hamburger").removeClass("is-active");
-    console.log("Class should be removed")
-    $("header, main, footer").removeClass("burger-shift-left");
-    $("header, main, footer").addClass("burger-shift-right");
-  } else {
-    $(".hamburger").addClass("is-active");
-    console.log("Class should be added")
-    $("header, main, footer").removeClass("burger-shift-right");
-    $("header, main, footer").addClass("burger-shift-left");
-    $(".mask-click").addClass("hamburger-mask");
-  };
+  $(".hamburger").addClass("is-active");
+  shiftContent.removeClass("burger-shift-right");
+  shiftContent.addClass("burger-shift-left");
+  $(".mask-click").addClass("hamburger-mask");
+  $("body").addClass("no-scroll");
+  $(".sb").css("transform", `${transformLeft}`);
+
+
 });
 
 $(".mask-click").click(function () {
-  if ($(".hamburger").hasClass("is-active")) {
-    $(".hamburger").removeClass("is-active");
-    console.log("Class should be removed")
-    $("header, main, footer").removeClass("burger-shift-left");
-    $("header, main, footer").addClass("burger-shift-right");
-    $(".mask-click").removeClass("hamburger-mask");
-  } else {
-    $(".hamburger").addClass("is-active");
-    console.log("Class should be added")
-    $("header, main, footer").removeClass("burger-shift-right");
-    $("header, main, footer").addClass("burger-shift-left");
-    $(".mask-click").addClass("hamburger-mask");
-  };
+  $(".hamburger").removeClass("is-active");
+  shiftContent.removeClass("burger-shift-left");
+  shiftContent.addClass("burger-shift-right");
+  $(".mask-click").removeClass("hamburger-mask");
+  $("body").removeClass("no-scroll");
+  $(".sb").css("transform", `0`);
+
 });
--- js/cookie.js
+++ js/cookie.js
@@ -1,60 +1,74 @@
-if(localStorage.getItem('cookieSeen') != 'shown'){
-    $(".cookie").delay(2000).fadeIn();
-    localStorage.setItem('cookieSeen','shown')
-};
-
 $(".close").click(function() {
   $(".pref__wrapper").delay(200).fadeOut();
   $(".cookie").fadeOut();
-  console.log("Close Clicked")
-  localStorage.setItem('cookieSeen','shown')
+  $("body").removeClass("no-scroll");
 });
 
 $(".preferences").click(function() {
   $(".pref__wrapper").fadeIn();
-  console.log("Preferences Clicked")
 });
 
 $(".cancel").click(function() {
   $(".pref__wrapper").fadeOut();
-  console.log("Cancel Clicked")
 });
 
-$(".pref__sect-2-btn-1").click(function () {
-  if ($(".pref__sect-2-btn-1").hasClass("pref--btn-white")) {
-  $(".pref__sect-2-btn-1").toggleClass('pref--btn-white pref--btn-purple')
-  $(".pref__sect-2-btn-2").toggleClass('pref--btn-purple pref--btn-white')
-  } else {
-  console.log("button 1 pressed")
-  };
-});
+const classToggles = 'pref--btn-white pref--btn-purple';
+function registerToggleButton(thisButton, otherButton) {
+    $(thisButton).click(function () {
+        if($(thisButton).hasClass('pref--btn-white')) {
+            $(thisButton).toggleClass(classToggles);
+            $(otherButton).toggleClass(classToggles);
+        };
+    });
+};
 
-$(".pref__sect-2-btn-2").click(function () {
-  if ($(".pref__sect-2-btn-2").hasClass("pref--btn-white")) {
-  $(".pref__sect-2-btn-2").toggleClass('pref--btn-white pref--btn-purple')
-  $(".pref__sect-2-btn-1").toggleClass('pref--btn-purple pref--btn-white')
-  } else {
-  console.log("button 1 pressed")
-  };
-});
+registerToggleButton(".pref__sect-2-btn-1", ".pref__sect-2-btn-2");
+registerToggleButton(".pref__sect-2-btn-2", ".pref__sect-2-btn-1");
 
-$(".pref__sect-3-btn-1").click(function () {
-  if ($(".pref__sect-3-btn-1").hasClass("pref--btn-white")) {
-  $(".pref__sect-3-btn-1").toggleClass('pref--btn-white pref--btn-purple')
-  $(".pref__sect-3-btn-2").toggleClass('pref--btn-purple pref--btn-white')
-  } else {
-  console.log("button 1 pressed")
-  };
-});
+registerToggleButton(".pref__sect-3-btn-1", ".pref__sect-3-btn-2");
+registerToggleButton(".pref__sect-3-btn-2", ".pref__sect-3-btn-1");
 
-$(".pref__sect-3-btn-2").click(function () {
-  if ($(".pref__sect-3-btn-2").hasClass("pref--btn-white")) {
-  $(".pref__sect-3-btn-2").toggleClass('pref--btn-white pref--btn-purple')
-  $(".pref__sect-3-btn-1").toggleClass('pref--btn-purple pref--btn-white')
-  } else {
-  console.log("button 1 pressed")
-  };
-});
+// if(localStorage.getItem('cookieSeen') != 'shown'){
+//     $(".cookie").delay(2000).fadeIn();
+//     localStorage.setItem('cookieSeen','shown')
+// };
+//
+//
+// $(".pref__sect-2-btn-1").click(function () {
+//   if ($(".pref__sect-2-btn-1").hasClass("pref--btn-white")) {
+//   $(".pref__sect-2-btn-1").toggleClass('pref--btn-white pref--btn-purple')
+//   $(".pref__sect-2-btn-2").toggleClass('pref--btn-purple pref--btn-white')
+//   } else {
+//   console.log("button 1 pressed")
+//   };
+// });
+//
+// $(".pref__sect-2-btn-2").click(function () {
+//   if ($(".pref__sect-2-btn-2").hasClass("pref--btn-white")) {
+//   $(".pref__sect-2-btn-2").toggleClass('pref--btn-white pref--btn-purple')
+//   $(".pref__sect-2-btn-1").toggleClass('pref--btn-purple pref--btn-white')
+//   } else {
+//   console.log("button 1 pressed")
+//   };
+// });
+//
+// $(".pref__sect-3-btn-1").click(function () {
+//   if ($(".pref__sect-3-btn-1").hasClass("pref--btn-white")) {
+//   $(".pref__sect-3-btn-1").toggleClass('pref--btn-white pref--btn-purple')
+//   $(".pref__sect-3-btn-2").toggleClass('pref--btn-purple pref--btn-white')
+//   } else {
+//   console.log("button 1 pressed")
+//   };
+// });
+//
+// $(".pref__sect-3-btn-2").click(function () {
+//   if ($(".pref__sect-3-btn-2").hasClass("pref--btn-white")) {
+//   $(".pref__sect-3-btn-2").toggleClass('pref--btn-white pref--btn-purple')
+//   $(".pref__sect-3-btn-1").toggleClass('pref--btn-purple pref--btn-white')
+//   } else {
+//   console.log("button 1 pressed")
+//   };
+// });
 
 // function setupEnableDisable(button, callback) {
 //   let enable = $(button).hasClass("pref--btn-white");
